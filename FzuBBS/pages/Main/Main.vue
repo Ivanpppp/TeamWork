3f107817f8267677cd2647c0ba94ed82f4ad8533
Ivanpppp <55040118+Ivanpppp@users.noreply.github.com>
2019-11-06 21:54:13 +0800

    update2

--- FzuBBS/pages/Main/Main.vue
+++ FzuBBS/pages/Main/Main.vue
@@ -1,11 +1,15 @@
 <template>
 	<view class="page">
-		
+		<topSearch></topSearch>
 	</view>
 </template>
 
 <script>
+	import topSearch from "../../components/topSearch.vue"
 	export default {
+		components:{
+			topSearch
+		},
 		data() {
 			return {
 				
