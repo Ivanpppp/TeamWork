9235071517f1dedd3ebaa361d76a34575df33796
Ivanpppp <55040118+Ivanpppp@users.noreply.github.com>
2019-11-06 11:53:48 +0800

    初始板块

---
+++ FzuBBS/pages/Main/Main.vue
@@ -0,0 +1,22 @@
+<template>
+	<view class="">
+		这是主页
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
