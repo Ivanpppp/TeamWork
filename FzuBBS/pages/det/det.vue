96588c76841209e23a987473bef10feb6e4dcae2
578983930 <578983930@qq.com>
2019-11-08 16:31:44 +0800

    版块、帖子、帖子详情页

---
+++ FzuBBS/pages/det/det.vue
@@ -0,0 +1,22 @@
+<template>
+	<view class="page">
+		帖子详情页
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
