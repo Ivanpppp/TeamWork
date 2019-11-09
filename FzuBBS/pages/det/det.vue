18c71ddd16121408e6d5cca6def7d8fa7f0a2fa9
578983930 <578983930@qq.com>
2019-11-09 21:16:03 +0800

    1

--- FzuBBS/pages/det/det.vue
+++ FzuBBS/pages/det/det.vue
@@ -23,38 +23,5 @@
 </script>
 
 <style>
-.title{
-	margin-top: 50upx;
-	font-size: 45upx;
-	font-weight: 700;
-	align-items: center;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	margin-bottom: 30upx;
-}
-.content{
-	width: 650upx;
-	height: 60upx;
-	margin-bottom: 20upx;
-	margin-left: 20upx;
-	border-radius: 20upx;
-}
-.btn{
-	width: 150upx;
-	height: 60upx;
-	margin-top: 100upx;
-	border-radius: 20upx;
-	align-items: center;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-}
-.texta{
-	width: 650upx;
-	height: 80upx;
-	margin-bottom: 20upx;
-	margin-left: 20upx;
-	border-radius: 20upx;
-}
+    @import url("../det/det.css");
 </style>
